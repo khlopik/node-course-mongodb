a372a3f836743eaf7d68ce3b12ca4d214f240199
Oleksii Khlopotov <khlopik@gmail.com>
2018-04-12 11:56:56 +0300

    added async functions

--- server/server.js
+++ server/server.js
@@ -122,7 +122,6 @@ app.post('/users', (req, res) => {
 	user.save()
 		.then(() => {
 			return user.generateAuthToken();
-			// res.send({user});
 		})
 		.then(token => {
 			res.header('x-auth', token).status(200).send(user);
@@ -137,19 +136,39 @@ app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user);
 });
 
-app.post('/users/login', (req, res) => {
-	let body = _.pick(req.body, ['email', 'password']);
+app.delete('users/me/token', authenticate, async (req, res) => {
+	try {
+		await req.user.removeToken(req.token);
+		res.status(200).send();
+	} catch(e) {
+		res.status(400).send();
+	}
+});
 
-	User.findByCredentials(body.email, body.password)
-		.then(user => {
-			return user.generateAuthToken()
-				.then(token => {
-					res.header('x-auth', token).send(user);
-				});
-		})
-		.catch(e => {
-			res.status(400).send(e);
-		})
+// app.post('/users/login', (req, res) => {
+// 	let body = _.pick(req.body, ['email', 'password']);
+//
+// 	User.findByCredentials(body.email, body.password)
+// 		.then(user => {
+// 			return user.generateAuthToken()
+// 				.then(token => {
+// 					res.header('x-auth', token).send(user);
+// 				});
+// 		})
+// 		.catch(e => {
+// 			res.status(400).send(e);
+// 		})
+// });
+
+app.post('/users/login', async (req, res) => {
+	try {
+		let body = _.pick(req.body, ['email', 'password']);
+		const user = await User.findByCredentials(body.email, body.password);
+		const token = await user.generateAuthToken();
+		res.header('x-auth', token).send(user);
+	} catch(e) {
+		res.status(400).send(e);
+	}
 });
 
 app.delete('/users/me/token', authenticate, (req, res) => {
